e92d2436ae79a71cbff1d35782cff540370c95cf
vivekDebugs <vivekkumar.1644@gmail.com>
2021-08-12 13:46:32 +0530

    formats code

--- src/App.js
+++ src/App.js
@@ -1,151 +1,154 @@
 import React, { useState, useRef, useEffect } from 'react'
-import './App.css';
-import Tab from './Tab';
+import './App.css'
+import Tab from './Tab'
 import { BsCaretLeftFill } from 'react-icons/bs'
 import { BsCaretRightFill } from 'react-icons/bs'
 import { FaPlus } from 'react-icons/fa'
 
 function App() {
+	const randomId = () => {
+		return Math.floor(Math.random() * 100000)
+	}
 
-  const randomId = () => {
-    return Math.floor(Math.random() * 100000)
-  }
+	const allTabs = [
+		{
+			id: randomId(),
+			name: 'Tab 1',
+			content: 'Tab 1 Content',
+		},
+	]
 
-  const allTabs = [
-    {
-      id : randomId(),
-      name : 'Tab 1',
-      content : 'Tab 1 Content'
-    },
-  ]
+	const [count, setCount] = useState(4)
+	const [tabs, setTabs] = useState(allTabs)
+	const [activeTab, setActiveTab] = useState(tabs[0])
+	const [showChevron, setShowChevron] = useState(false)
+	const ref = useRef(null)
 
-  const [count, setCount] = useState(4)
-  const [tabs, setTabs] = useState(allTabs)
-  const [activeTab, setActiveTab] = useState(tabs[0])
-  const [showChevron, setShowChevron] = useState(false)
-  const ref = useRef(null)
+	const _setTabs = arr => {
+		setTabs(arr)
+	}
 
-  const _setTabs = (arr) => {
-    setTabs(arr)
-  }
+	const _setActiveTab = obj => {
+		setActiveTab(obj)
+	}
 
-  const _setActiveTab = (obj) => {
-    setActiveTab(obj)
-  }
+	useEffect(() => {
+		const createTabs = () => {
+			const _tabs = []
+			for (let i = 1; i < 4; i++) {
+				const _newTab = {
+					id: randomId(),
+					name: `Tab ${i}`,
+					content: `Tab ${i} content`,
+				}
+				_tabs.push(_newTab)
+			}
+			_setTabs(_tabs)
+			_setActiveTab(_tabs[0])
+		}
 
-  useEffect(() => {
-    const createTabs = () => {
-      const _tabs = []
-      for (let i = 1; i < 4; i++) {
-        const _newTab = {
-          id : randomId(),
-          name : `Tab ${i}`,
-          content : `Tab ${i} content`
-        }
-        _tabs.push(_newTab)
-      }
-      _setTabs(_tabs)
-      _setActiveTab(_tabs[0])
-    }
+		createTabs()
+	}, [])
 
-    createTabs()
-    
-  }, [])
+	const closeTab = (tab, e) => {
+		e.stopPropagation()
+		if (tabs.length > 1) {
+			const close = window.confirm('This action will close the tab.')
+			if (close) {
+				const newTabs = [...tabs]
+				const index = newTabs.indexOf(tab)
+				if (activeTab === tab) {
+					const newActive = index === 0 ? 1 : index - 1
+					setActiveTab(tabs[newActive])
+				}
+				newTabs.splice(index, 1)
+				setTabs(newTabs)
+			}
+		} else {
+			return
+		}
+	}
 
-  const closeTab = (tab, e) => {
-    e.stopPropagation()
-    if (tabs.length > 1) {
-      const close = window.confirm('This action will close the tab.')
-      if (close) {
-        const newTabs = [...tabs]
-        const index = newTabs.indexOf(tab)
-        if (activeTab === tab) {
-          const newActive = index === 0 ? 1 : index - 1;
-          setActiveTab(tabs[newActive])
-        }
-        newTabs.splice(index, 1)
-        setTabs(newTabs)
-      }
-    } else {
-      return
-    }
-  }
+	const addTab = () => {
+		if (tabs.length < 10) {
+			const newTabs = [...tabs]
+			setCount(count + 1)
+			const newTab = {
+				id: randomId(),
+				name: `Tab ${count}`,
+				content: `Tab ${count} content`,
+			}
+			newTabs.push(newTab)
+			setTabs(newTabs)
+			setActiveTab(newTab)
+		} else {
+			return
+		}
+	}
 
-  const addTab = () => {
-    if (tabs.length < 10) {
-      const newTabs = [...tabs]
-      setCount(count + 1)
-      const newTab = {
-        id : randomId(),
-        name : `Tab ${count}` ,
-        content : `Tab ${count} content`
-      }
-      newTabs.push(newTab)
-      setTabs(newTabs)
-      setActiveTab(newTab)
-    } else {
-      return      
-    }
-  }
+	const handleScroll = offset => {
+		ref.current.scrollLeft += offset
+	}
 
-  const handleScroll = (offset) => {
-    ref.current.scrollLeft += offset;
-  }
+	const isOverflow = el => {
+		return el.scrollWidth > el.offsetWidth
+	}
 
-  const isOverflow = (el) => {
-    return el.scrollWidth > el.offsetWidth
-  }
+	const _setShow = val => {
+		setShowChevron(val)
+	}
 
-  const _setShow = (val) => {
-    setShowChevron(val)
-  }
+	const handleActive = id => {
+		const item = tabs.find(tab => tab.id === id)
+		setActiveTab(item)
+	}
 
-  const handleActive = (id) => {
-    const item = tabs.find(tab => tab.id === id)
-    setActiveTab(item)
-  }
+	useEffect(() => {
+		const container = document.getElementById('tabsContainer')
+		if (isOverflow(container)) {
+			_setShow(true)
+		} else {
+			_setShow(false)
+		}
+	})
 
-  useEffect(() => {
-    const container = document.getElementById('tabsContainer')
-    if (isOverflow(container)) {
-      _setShow(true)      
-    } else {
-      _setShow(false)
-    }
-  })
-  
-  return (
-    <div className="app">
-        <div className="app__container">
-          <div className="app__tabsContainer" ref={ref} id='tabsContainer'>
-            {tabs.map((tab, index) => (
-              <Tab 
-              tab={tab} key={tab.id} 
-              closeTab={closeTab}
-              handleActive={handleActive}
-              isActive={activeTab === tab ? true : false}
-              />
-            ))}
-            {showChevron && (
-              <>
-              <BsCaretLeftFill 
-              className={`app__chevronLeft icon`}
-              onClick={() => handleScroll(-20)} />
-              <BsCaretRightFill 
-              className='app__chevronRight icon' 
-              onClick={() => handleScroll(20)} />
-              </>
-            )}
-            <FaPlus className='app__plusIcon icon' onClick={addTab} />
-          </div>
-          <div className="app__contentContainer">
-            <p>
-              {activeTab.content}
-            </p>
-          </div>
-        </div>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<div className='app__container'>
+				<div
+					className='app__tabsContainer'
+					ref={ref}
+					id='tabsContainer'
+				>
+					{tabs.map((tab, index) => (
+						<Tab
+							tab={tab}
+							key={tab.id}
+							closeTab={closeTab}
+							handleActive={handleActive}
+							isActive={activeTab === tab ? true : false}
+						/>
+					))}
+					{showChevron && (
+						<>
+							<BsCaretLeftFill
+								className={`app__chevronLeft icon`}
+								onClick={() => handleScroll(-20)}
+							/>
+							<BsCaretRightFill
+								className='app__chevronRight icon'
+								onClick={() => handleScroll(20)}
+							/>
+						</>
+					)}
+					<FaPlus className='app__plusIcon icon' onClick={addTab} />
+				</div>
+				<div className='app__contentContainer'>
+					<p>{activeTab.content}</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/Tab.jsx
+++ src/Tab.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './Tab.css'
-import { GrClose } from  'react-icons/gr'
+import { GrClose } from 'react-icons/gr'
 
 const Tab = ({ tab, closeTab, handleActive, isActive }) => {
-    return (
-        <div className={`tab ${isActive && 'active'}`} onClick={() => handleActive(tab.id)}>
-          <span>{tab.name}</span>
-          <GrClose className='tab__close' onClick={(e) => closeTab(tab, e)} />
-        </div>
-    )
+	return (
+		<div
+			className={`tab ${isActive && 'active'}`}
+			onClick={() => handleActive(tab.id)}
+		>
+			<span>{tab.name}</span>
+			<GrClose className='tab__close' onClick={e => closeTab(tab, e)} />
+		</div>
+	)
 }
 
 export default Tab
